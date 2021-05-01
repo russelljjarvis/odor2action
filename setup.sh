ca5dcb8915f0222902eaf95f30c6671c07d3059d
Russell Jarvis <russelljarvis@protonmail.com>
2021-05-01 22:01:03 +1000

    update

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@
 sudo apt-get update
 sudo apt-get upgrade
 #sudo apt-get install graphviz graphviz-dev
-sudo conda install -c cython# bokeh graphviz_layout
-#sudo python3 -m pip install numpy --upgrade --ignore-installed
+#sudo conda install -c cython# bokeh graphviz_layout
+sudo python3 -m pip install numpy --upgrade --ignore-installed
+sudo python3 -m pip install cython --upgrade --ignore-installed
+
 sudo python3 -m pip install -r requirements.txt
 sudo python3 -m pip install seaborn holoviews bokeh
 sudo python3 -m pip install pyvis dash_bio cython
@@ -11,9 +13,9 @@ sudo python3 -m pip install dask plotly tabulate# streamlit-agraph
 #git clone https://github.com/pygraphviz/pygraphviz; cd pygraphviz; sudo python3 setup.py install; cd -
 sudo python3 -m conda install -c pyvis holoviews bokeh seaborn dash_bio
 sudo conda install -c pyviz holoviews# bokeh graphviz_layout
-sudo python3 -m pip install git+https://github.com/pyviz/holoviews.git
-git clone https://github.com/pyviz/holoviews.git
-cd holoviews; sudo pip install -e .; cd ..;
+#sudo python3 -m pip install git+https://github.com/pyviz/holoviews.git
+#git clone https://github.com/pyviz/holoviews.git
+#cd holoviews; sudo pip install -e .; cd ..;
 #sudo python3 -m pip install pygraphviz
 mkdir -p ~/.streamlit/
 echo "\

b38f6a755d53cd2aa2f0c408217360fdf981d08c
Russell Jarvis <russelljarvis@protonmail.com>
2021-05-01 22:33:26 +1000

    update

--- setup.sh
+++ setup.sh
@@ -8,12 +8,13 @@ sudo python3 -m pip install numpy --upgrade --ignore-installed
 sudo python3 -m pip install cython --upgrade --ignore-installed
 
 sudo python3 -m pip install -r requirements.txt
+sudo conda install -c pyviz holoviews scikit-image# bokeh graphviz_layout
+sudo python3 -m conda install -c pyvis holoviews bokeh seaborn dash_bio scikit-image
 sudo python3 -m pip install seaborn holoviews bokeh
 sudo python3 -m pip install pyvis dash_bio cython scikit-image
 sudo python3 -m pip install dask plotly tabulate# streamlit-agraph
+python3 -c "from holoviews.operation.datashader import datashade"
 #git clone https://github.com/pygraphviz/pygraphviz; cd pygraphviz; sudo python3 setup.py install; cd -
-sudo python3 -m conda install -c pyvis holoviews bokeh seaborn dash_bio scikit-image
-sudo conda install -c pyviz holoviews scikit-image# bokeh graphviz_layout
 #sudo python3 -m pip install git+https://github.com/pyviz/holoviews.git
 #git clone https://github.com/pyviz/holoviews.git
 #cd holoviews; sudo pip install -e .; cd ..;

312d6f1324bcaeb3231b7c89dd58d62da757d4ef
Russell Jarvis <russelljarvis@protonmail.com>
2021-05-25 12:43:38 +1000

    update

--- setup.sh
+++ setup.sh
@@ -1,32 +1,32 @@
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo python -m pip install -U pip
-#sudo apt-get install graphviz graphviz-dev
-#sudo conda install -c cython# bokeh graphviz_layout
-sudo python3 -m pip install numpy --upgrade --ignore-installed
-sudo python3 -m pip install cython --upgrade --ignore-installed
-sudo $(which python) -m pip install git+https://github.com/taynaud/python-louvain.git@networkx2
-sudo python3 -m pip install git+https://github.com/taynaud/python-louvain.git@networkx2
+apt-get update
+apt-get upgrade
+python -m pip install -U pip
+#apt-get install graphviz graphviz-dev
+#conda install -c cython# bokeh graphviz_layout
+python3 -m pip install numpy --upgrade --ignore-installed
+python3 -m pip install cython --upgrade --ignore-installed
+$(which python) -m pip install git+https://github.com/taynaud/python-louvain.git@networkx2
+python3 -m pip install git+https://github.com/taynaud/python-louvain.git@networkx2
 
-sudo python3 -m pip install -r requirements.txt
-sudo conda install -c pyviz scikit-image# bokeh graphviz_layout
-sudo python3 -m conda install -c pyvis bokeh seaborn dash_bio scikit-image
-sudo python3 -m pip install seaborn bokeh# holoviews==1.14.1
-sudo python3 -m pip install pyvis dash_bio cython scikit-image
-sudo python3 -m pip install dask plotly tabulate bokeh hiveplotlib hiveplot pygraphviz#==2.0.0#2.2
-sudo python3 -m pip install streamlit --upgrade --ignore-installed
-sudo pip install hiveplotlib pygraphviz
+python3 -m pip install -r requirements.txt
+conda install -c pyviz scikit-image# bokeh graphviz_layout
+python3 -m conda install -c pyvis bokeh seaborn dash_bio scikit-image
+python3 -m pip install seaborn bokeh# holoviews==1.14.1
+python3 -m pip install pyvis dash_bio cython scikit-image
+python3 -m pip install dask plotly tabulate bokeh hiveplotlib hiveplot pygraphviz#==2.0.0#2.2
+python3 -m pip install streamlit --upgrade --ignore-installed
+pip install hiveplotlib pygraphviz
 # streamlit-agraph
-sudo $(which pip) install python-igraph
-sudo $(which pip) install git+https://github.com/taynaud/python-louvain.git@networkx2
-git clone https://github.com/taynaud/python-louvain.git@networkx2; cd networkx; sudo $(which python) setup.py install; cd -
+$(which pip) install python-igraph
+$(which pip) install git+https://github.com/taynaud/python-louvain.git@networkx2
+git clone https://github.com/taynaud/python-louvain.git@networkx2; cd networkx2; $(which python) setup.py install; cd -
 python3 -c "from holoviews.operation.datashader import datashade"
-git clone https://github.com/pygraphviz/pygraphviz; cd pygraphviz; sudo python3 setup.py install; cd -
-#sudo python3 -m pip install git+https://github.com/pyviz/holoviews.git
+git clone https://github.com/pygraphviz/pygraphviz; cd pygraphviz; python3 setup.py install; cd -
+#python3 -m pip install git+https://github.com/pyviz/holoviews.git
 #git clone https://github.com/pyviz/holoviews.git
-#cd holoviews; sudo pip install -e .; cd ..;
-#sudo python3 -m pip install pygraphviz
+#cd holoviews; pip install -e .; cd ..;
+#python3 -m pip install pygraphviz
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\

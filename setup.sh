33bdd7b6e1a5beec4a9798a0281578fd90e5e36c
Russell Jarvis <russelljarvis@protonmail.com>
2021-05-25 13:09:42 +1000

    update slimmer

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 apt-get update
 apt-get upgrade
 #apt-get install -y graphviz #| grep http | awk '{print $1}' | tr -d "'"
+# git clone https://github.com/pygraphviz/pygraphviz; cd pygraphviz; python3 setup.py install; cd -
+
 # Create .profile.d script (sourced by dyno) setting PATH and LD_LIBRARY_PATH
 #mkdir -p .profile.d
 #echo "PATH=/app/$install_dir/usr/bin:\$PATH" >.profile.d/graphviz.sh
@@ -21,18 +23,17 @@ python3 -m pip install git+https://github.com/taynaud/python-louvain.git@network
 
 python3 -m pip install -r requirements.txt
 #conda install -c pyviz scikit-image# bokeh graphviz_layout
-python3 -m conda install -c pyvis bokeh seaborn dash_bio scikit-image
+python3 -m conda install -c pyvis bokeh seaborn scikit-image # dash_bio
 python3 -m pip install seaborn bokeh# holoviews==1.14.1
 python3 -m pip install pyvis dash_bio cython scikit-image
-python3 -m pip install dask plotly tabulate bokeh hiveplotlib hiveplot pygraphviz#==2.0.0#2.2
+python3 -m pip install dask plotly tabulate bokeh# hiveplotlib hiveplot pygraphviz#==2.0.0#2.2
 python3 -m pip install streamlit --upgrade --ignore-installed
-python3 -m pip install hiveplotlib pygraphviz
+#python3 -m pip install pygraphviz
 # streamlit-agraph
 $(which pip) install python-igraph
 $(which pip) install git+https://github.com/taynaud/python-louvain.git@networkx2
 git clone https://github.com/taynaud/python-louvain.git@networkx2; cd networkx2; $(which python) setup.py install; cd -
 python3 -c "from holoviews.operation.datashader import datashade"
-git clone https://github.com/pygraphviz/pygraphviz; cd pygraphviz; python3 setup.py install; cd -
 #python3 -m pip install git+https://github.com/pyviz/holoviews.git
 #git clone https://github.com/pyviz/holoviews.git
 #cd holoviews; pip install -e .; cd ..;

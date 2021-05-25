44687e9e46b7c2151ca3e44ab8710b281711d8ed
Russell Jarvis <russelljarvis@protonmail.com>
2021-05-25 15:20:54 +1000

    update

--- setup.sh
+++ setup.sh
@@ -1,17 +1,6 @@
 
 apt-get update
-apt-get upgrade
-#apt-get install -y graphviz #| grep http | awk '{print $1}' | tr -d "'"
-# git clone https://github.com/pygraphviz/pygraphviz; cd pygraphviz; python setup.py install; cd -
-
-# Create .profile.d script (sourced by dyno) setting PATH and LD_LIBRARY_PATH
-#mkdir -p .profile.d
-#echo "PATH=/app/$install_dir/usr/bin:\$PATH" >.profile.d/graphviz.sh
-#echo "export LD_LIBRARY_PATH=/app/$libs_dir:\$LD_LIBRARY_PATH" >>.profile.d/graphviz.sh
-
-# Run dot -c to configure plugins (creates config6a file)
-#LD_LIBRARY_PATH=$libs_dir "$install_dir"/usr/bin/dot -c
-
+#apt-get upgrade
 
 python -m pip install -U pip
 #apt-get install graphviz graphviz-dev
@@ -22,21 +11,22 @@ python -m pip install cython --upgrade --ignore-installed
 python -m pip install -r requirements.txt
 #conda install -c pyviz scikit-image# bokeh graphviz_layout
 #python -m conda install -c pyvis bokeh seaborn scikit-image # dash_bio
-python -m pip install bokeh# holoviews==1.14.1 seaborn
+#python -m pip install bokeh# holoviews==1.14.1 seaborn
 python -m pip install pyvis cython scikit-image #dash_bio dask
 python -m pip install plotly tabulate # hiveplotlib hiveplot pygraphviz#==2.0.0#2.2
 python -m pip install streamlit --upgrade --ignore-installed
 #python -m pip install pygraphviz
 # streamlit-agraph
-$(which pip) install python-igraph
+python -m pip install python-igraph
 
 #$(which python) -m pip install git+https://github.com/taynaud/python-louvain.git@networkx2
 #python -m pip install git+https://github.com/taynaud/python-louvain.git@networkx2
 
 
-$(which pip) install git+https://github.com/taynaud/python-louvain.git@networkx2
-git clone https://github.com/taynaud/python-louvain.git@networkx2; cd networkx2; $(which python) setup.py install; cd -
-
+python -m pip install git+https://github.com/taynaud/python-louvain.git@networkx2
+git clone https://github.com/taynaud/python-louvain.git@networkx2; cd networkx2; python setup.py install; cd -
+python make_serial_plots0.py
+python make_serial_plots1.py
 #python -c "from holoviews.operation.datashader import datashade"
 #python -m pip install git+https://github.com/pyviz/holoviews.git
 #git clone https://github.com/pyviz/holoviews.git

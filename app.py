73b48d4b371d5aec3f0a05787542ffe211090893
Russell Jarvis <russelljarvis@protonmail.com>
2021-12-23 15:52:58 +1100

    adding in speed cache

--- app.py
+++ app.py
@@ -601,10 +601,7 @@ def learn_embeddings(walks):
 # @st.cache(persist=True)
 def get_table_download_link_csv(df):
     import base64
-
-    # csv = df.to_csv(index=False)
     csv = df.to_csv().encode()
-    # b64 = base64.b64encode(csv.encode()).decode()
     b64 = base64.b64encode(csv).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="captura.csv" target="_blank">Download csv file</a>'
     return href
